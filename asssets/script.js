28bec0769ad47fefb0357946b45c9e44bd81156f
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-10-07 15:32:48 -0400

    started working on five day forecast. Stuck on getting input the response of my api into my html via vsc

--- asssets/script.js
+++ asssets/script.js
@@ -2,33 +2,7 @@
 var res;
 
 
-// We store all of the retrieved data inside of an object called "response"
-// .then(function (response) {
-
-//     // Log the queryURL
-//     console.log(queryURL);
-
-//     // Log the resulting object
-//     console.log(response);
-
-
-//     // Transfer content to HTML
-//     $("#city").html("<h1>" + response.name + " Weather Details</h1>");
-
-//     // add temp content to html
-//     // $("#temp").text("Temperature (K) " + response.main.temp);
-//     $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-//     // Transfer content to HTML
-//     $("#humidity").text("Humidity: " + response.main.humidity);
-//     $("#wind").text("Wind Speed: " + response.wind.speed);
-
-
-//     // Log the data in the console as well
-//     console.log("Wind Speed: " + response.wind.speed);
-//     console.log("Humidity: " + response.main.humidity);
-//     console.log("Temperature (F): " + tempF);
-// });
+// let cityDate = moment().format('LLL');
 
 
 document.getElementById("search").addEventListener("click", searchWeatherByCity);
@@ -36,22 +10,56 @@ document.getElementById("search").addEventListener("click", searchWeatherByCity)
 function searchWeatherByCity() {
     var searchTerm = document.getElementById("searchTerm");
     // URL we need to quuery the database
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&appid=${APIKey}&units=imperial`
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&appid=${APIKey}&units=imperial`;
 
+    var fiveDayForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm.value}&appid=${APIKey}&units=imperial`;
 
+    // AJAx API calls
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response) {
-        console.log(response);
         res = response;
+    
+    $.ajax({
+        url: fiveDayForecastURL,
+        method: "GET"
+    })
+    
+    .then(function(response) {
+        res = response;
+    })
         
+        // Display name, temp and humidity
         document.getElementById("nameOfCity").innerText = res.name
         document.getElementById("temp").innerText = res.main.temp
-        document.getElementById("humidity").innerText = document.getElementById("humidity").innerText + res.wind.speed
-        
+        document.getElementById("humidity").innerText = res.main.humidity
+        document.getElementById("humidity").innerText = document.getElementById("humidity").innerText + res.main.humidity
+        document.getElementById("windSpeed").innerText = res.wind.speed
 
 
+        document.getElementById("dayOneDate").innerText = res.list[1].dt_txt
+        document.getElementById("dayOneTemp").innerText = res.list[1].main.temp
     })
+    searchedCities();
 }
+// Previously Searched Cities Saved to Local Storage //
+function searchedCities() {
+    let searchedCities = JSON.parse(localStorage.getItem("searchedCitiesContainer"));
+    if (searchedCities !== null) {
+    cities = searchedCities;
+    }
+}
+function storeCities() {
+    localStorage.setItem("searchedCitiesContainer", JSON.stringify(cities));
+}
+
+// function fiveDay() {
+//     // Display name, temp and humidity
+//     document.getElementById("dayOneDate").innerText = res.list[1].dt_txt
+//     document.getElementById("temp").innerText = res.main.temp
+//     document.getElementById("humidity").innerText = res.main.humidity
+//     document.getElementById("humidity").innerText = document.getElementById("humidity").innerText + res.main.humidity
+//     document.getElementById("windSpeed").innerText = res.wind.speed
+// }
